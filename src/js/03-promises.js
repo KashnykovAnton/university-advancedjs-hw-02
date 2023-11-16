0124b654253511b1bcb836ab98da0202354c9ae4
KashnykovAnton <80004672+KashnykovAnton@users.noreply.github.com>
2023-11-16 15:42:56 +0100

    promises added

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import iziToast from "izitoast"
+
+const refs = {
+  form: document.querySelector("form"),
+  inputs: document.querySelectorAll("input"),
+};
+
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let values = {};
+  [...refs.inputs].map((el) => (values[el.name] = Number(el.value)));
+  const { delay, step, amount } = values;
+
+  for (let i = 0; i < amount; i++) {
+    let delayWithStep = delay + i * step;
+    setTimeout(() => {
+      createPromise(i + 1, delayWithStep)
+        .then(({ position, delay }) => {
+          showToast(`✅ Fulfilled promise ${position} in ${delay}ms`, "res");
+        })
+        .catch(({ position, delay }) => {
+          showToast(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, delayWithStep);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const result = { position, delay };
+  return new Promise((res, rej) => {
+    if (shouldResolve) {
+      res(result);
+    } else {
+      rej(result);
+    }
+  });
+}
+
+function showToast(text, string) {
+  iziToast.show({
+    title: "Notice",
+    message: text,
+    position: "topRight",
+    color: string ? "green" : "red",
+    transitionIn: "fadeInDown",
+  });
 }
